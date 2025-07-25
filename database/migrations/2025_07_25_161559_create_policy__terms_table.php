170f402e2f3daec25251f2956a7ceb341ac9f343
Mike Paez <mpaez0105@gmail.com>
2025-07-25 11:31:12 -0500

    jul25_6

--- database/migrations/2025_07_25_161559_create_policy__terms_table.php
+++ database/migrations/2025_07_25_161559_create_policy__terms_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('policy__terms', function (Blueprint $table) {
             $table->id();
-            $table->string ('policy')->nullable(false);
+            $table->string ('policy');
             $table->string ('term');
             $table->timestamps()->useCurrent();;
         });
